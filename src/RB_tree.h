403243382556e7c4c2e0ed6e793c5d565421a140
Artur Komissarov <komissarov.artur2009@gmail.com>
2024-05-08 21:33:16 +0300

    ini RB_tree

--- src/RB_tree.h
+++ src/RB_tree.h
@@ -3,9 +3,6 @@
 class RBTree {
 	struct Node {
 		Node(int val, Node* left = nullptr, Node* right = nullptr) : val(val), left(left), right(right) {};
-		// int height;
-		// std::string color = "red";
-		// int key;
 		int val;
 		Node* left;
 		Node* right;
@@ -97,7 +94,7 @@ public:
 		std::cout << std::endl;
     }
 private:
-    void preorder(Node *root){
+    void preorder(Node *root) {
 		if(!root) {
 			return;
 		}
@@ -107,4 +104,81 @@ private:
 
         preorder(root->right);
     }
+};
+
+
+enum class Color {black, red};
+class RBtree {
+	struct Node {
+		Node(int val_, Node* parent_ = nullptr, Node* left_ = nullptr, Node* right_ = nullptr) : val(val_), left(left_), right(right_), parent(parent_) {};
+		int val;
+		Node* left;
+		Node* right;
+		Color color = Color::red;
+		Node* parent;
+	};
+	Node* header = nullptr;
+	 void preorder(Node *root) {
+		if(!root) {
+			return;
+		}
+		std::cout << root->val << " ";
+
+        preorder(root->left);
+
+        preorder(root->right);
+    }
+public:
+RBtree(){
+	header = new Node(INT_MIN);
+}
+	void insert(int a) {
+		if(!header->right) {
+			header->right = new Node(a,header);
+			header->right->color = Color::black;
+			return;
+		}
+		Node *tmp = header->right;
+		while(tmp){
+			if(tmp->val > a) {
+				if(!tmp->left){
+					tmp->left = new Node(a,tmp);
+					tmp = tmp->left;
+					break;
+				}
+				tmp = tmp->left;
+			} else {
+				if(!tmp->right){
+					tmp->right = new Node(a,tmp);
+					tmp = tmp->right;
+					break;
+				}
+				tmp = tmp->right;
+			}
+		}
+		Node* grand = tmp->parent->parent;
+		if(grand) {
+			if(tmp->parent == grand->right) {
+				if(grand->left->color == Color::red) {
+					if(grand != header->right){
+						grand->color = Color::red;
+					}
+					grand->left->color = Color::black;
+				} else if(grand->left->color == Color::black) {
+					Node* brother = tmp->parent->left;
+					tmp->parent->left = grand;
+					tmp->parent->left->right = brother;
+					tmp->parent->parent = grand->parent;
+				}
+			} else {}
+		} 
+	}
+
+	void print() {
+        preorder(header->right);
+		std::cout << std::endl;
+    }
+
+	void erase(int a){}
+	bool find(int a){return false;}
 };
