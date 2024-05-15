d0a1120586a3427d349fc57b4b71123d1f484fd8
Artur Komissarov <komissarov.artur2009@gmail.com>
2024-05-15 19:22:02 +0300

    Delete prototype and decomposed insert

--- src/RB_tree.h
+++ src/RB_tree.h
@@ -1,113 +1,114 @@
 #include <iostream>
 #include <set>
-class RBTree {
-    struct Node {
-        Node(int val, Node* left = nullptr, Node* right = nullptr)
-            : val(val), left(left), right(right){};
-        int val;
-        Node* left;
-        Node* right;
-    };
-    Node* root = nullptr;
+// ПРОТОТИП
+// class RBTree {
+//     struct Node {
+//         Node(int val, Node* left = nullptr, Node* right = nullptr)
+//             : val(val), left(left), right(right){};
+//         int val;
+//         Node* left;
+//         Node* right;
+//     };
+//     Node* root = nullptr;
 
-   public:
-    void insert(int a) {
-        if (!root) {
-            root = new Node(a);
-            return;
-        }
-        Node* tmp = root;
-        if (root->val < a) {
-            while (tmp->right != nullptr) {
-                tmp = tmp->right;
-            }
-            tmp->right = new Node(a);
-        } else {
-            while (tmp->left != nullptr) {
-                tmp = tmp->left;
-            }
-            tmp->left = new Node(a);
-        }
-    }
+//    public:
+//     void insert(int a) {
+//         if (!root) {
+//             root = new Node(a);
+//             return;
+//         }
+//         Node* tmp = root;
+//         if (root->val < a) {
+//             while (tmp->right != nullptr) {
+//                 tmp = tmp->right;
+//             }
+//             tmp->right = new Node(a);
+//         } else {
+//             while (tmp->left != nullptr) {
+//                 tmp = tmp->left;
+//             }
+//             tmp->left = new Node(a);
+//         }
+//     }
 
-    void erase(int a) {
-        if (!root) {
-            return;
-        }
-        Node* tmp = root;
-        if (tmp->val < a) {
-            while (tmp != nullptr) {
-                if (tmp->right != nullptr) {
-                    if (tmp->right->val != a) {
-                        tmp = tmp->right;
-                    } else {
-                        Node* remem = tmp->right->right;
-                        delete tmp->right;
-                        tmp->right = remem;
-                    }
-                } else {
-                    return;
-                }
-            }
-        } else {
-            while (tmp != nullptr) {
-                if (tmp->left != nullptr) {
-                    if (tmp->left->val != a) {
-                        tmp = tmp->left;
-                    } else {
-                        Node* remem = tmp->left->left;
-                        delete tmp->left;
-                        tmp->left = remem;
-                    }
-                } else {
-                    return;
-                }
-            }
-        }
-    }
+//     void erase(int a) {
+//         if (!root) {
+//             return;
+//         }
+//         Node* tmp = root;
+//         if (tmp->val < a) {
+//             while (tmp != nullptr) {
+//                 if (tmp->right != nullptr) {
+//                     if (tmp->right->val != a) {
+//                         tmp = tmp->right;
+//                     } else {
+//                         Node* remem = tmp->right->right;
+//                         delete tmp->right;
+//                         tmp->right = remem;
+//                     }
+//                 } else {
+//                     return;
+//                 }
+//             }
+//         } else {
+//             while (tmp != nullptr) {
+//                 if (tmp->left != nullptr) {
+//                     if (tmp->left->val != a) {
+//                         tmp = tmp->left;
+//                     } else {
+//                         Node* remem = tmp->left->left;
+//                         delete tmp->left;
+//                         tmp->left = remem;
+//                     }
+//                 } else {
+//                     return;
+//                 }
+//             }
+//         }
+//     }
 
-    bool find(int a) {
-        if (!root) {
-            return false;
-        }
-        Node* tmp = root;
-        if (tmp->val > a) {
-            while (tmp != nullptr) {
-                if (a != tmp->val) {
-                    tmp = tmp->left;
-                } else {
-                    return true;
-                }
-            }
-        } else {
-            while (tmp != nullptr) {
-                if (a != tmp->val) {
-                    tmp = tmp->right;
-                } else {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+//     bool find(int a) {
+//         if (!root) {
+//             return false;
+//         }
+//         Node* tmp = root;
+//         if (tmp->val > a) {
+//             while (tmp != nullptr) {
+//                 if (a != tmp->val) {
+//                     tmp = tmp->left;
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         } else {
+//             while (tmp != nullptr) {
+//                 if (a != tmp->val) {
+//                     tmp = tmp->right;
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
 
-    void print() {
-        preorder(root);
-        std::cout << std::endl;
-    }
+//     void print() {
+//         preorder(root);
+//         std::cout << std::endl;
+//     }
 
-   private:
-    void preorder(Node* root) {
-        if (!root) {
-            return;
-        }
-        std::cout << root->val << " ";
+//    private:
+//     void preorder(Node* root) {
+//         if (!root) {
+//             return;
+//         }
+//         std::cout << root->val << " ";
 
-        preorder(root->left);
+//         preorder(root->left);
 
-        preorder(root->right);
-    }
-};
+//         preorder(root->right);
+//     }
+// };
 
 enum class Color { black, red };
 class RBtree {
@@ -137,26 +138,19 @@ class RBtree {
         if (!grand) {
             return;
         }
-
-        if (tmp->parent == grand->right) {
-            if (grand->left->color == Color::red) {
-                if (grand != header->right) {
-                    grand->color = Color::red;
-                }
-                grand->left->color = Color::black;
-            } else if (grand->left->color == Color::black) {
-                Node* brother = tmp->parent->left;
-                // Перестраиваем дерево
-                tmp->parent->left = grand;
-                tmp->parent->left->right = brother;
-                // Меняем зависимость узлов(родителей и предков)
-                tmp->parent->parent = grand->parent;
-                tmp->parent->left->parent = tmp->parent;
-                brother->parent = grand;
-                // Перекрашеваем
-                tmp->parent->left->color = Color::red;
-                tmp->parent->color = Color::black;
-            }
+        if (tmp->parent == grand->right && tmp->parent->color == Color::red &&
+            grand->right->right == tmp) {
+            Node* brother = tmp->parent->left;
+            // Перестраиваем дерево
+            tmp->parent->left = grand;
+            tmp->parent->left->right = brother;
+            // Меняем зависимость узлов(родителей и предков)
+            tmp->parent->parent = grand->parent;
+            tmp->parent->left->parent = tmp->parent;
+            brother->parent = grand;
+            // Перекрашеваем
+            tmp->parent->left->color = Color::red;
+            tmp->parent->color = Color::black;
         } else {
         }
     }
